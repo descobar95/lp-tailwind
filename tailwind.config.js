896c7bd235a1adaa06b42c4dc9d77e5e943c35b8
Danilo Escobar <danilo_es@hotmail.es>
2022-03-02 15:18:54 -0600

    lp finished

---
+++ tailwind.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  purge: [
+    './public/**/*.html',
+  ],
+  content: [
+    './public/**/*.{html,js}',
+    './public/*.{html,js}'
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      backgroundImage: {
+        "bali": "url('../img/bali.jpg')",
+        "chicago": "url('../img/chicago.jpg')",
+        "europe": "url('../img/europe.jpg')",
+        "iceland": "url('../img/iceland.jpg')",
+        "la": "url('../img/LA.jpg')",
+        "miami": "url('../img/miami.jpg')",
+        "newYork": "url('../img/new_york.jpg')",
+        "norway": "url('../img/norway.jpg')",
+        "sanFrancisco": "url('../img/sanFrancisco.jpg')",
+        "sanFranciscoDesktop": "url('../img/sanFranciscoDesktop.jpg')",
+        "seattle": "url('../img/seattle.jpg')",
+        "switzerland": "url('../img/switzerland.jpg')",
+        "sydney": "url('../img/sydney.jpg')",
+        "yosemite": "url('../img/yosemite.jpg')"
+      },
+      backgroundColor: theme => ({
+        ...theme('colors'),
+        'primary': '#CC2D4A',
+        'secondary': '#8FA206',
+        'terciary': '#61AEC9'
+      }),
+      textColor: {
+        'primary': '#CC2D4A',
+        'secondary': '#8FA206',
+        'terciary': '#61AEC9'
+      },
+      fontFamily: {
+        Montserrat: ['Montserrat', 'sans-serif']
+      }
+    },
+  },
+  variants: {
+    width: ["responsive", "hover", "focus"],
+  },
+  plugins: [],
+}
